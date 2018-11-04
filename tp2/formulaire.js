cb17349dd85093032b6011e0c14833dc368056fb
oueriemmi <44141988+oueriemmi@users.noreply.github.com>
2018-11-04 23:47:50 +0100

    tp2essai
    tp2essai

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,35 +1,57 @@
-<script>        
-     function validation() { 
-         if(document.getElementById("nom").value==""||5=< document.getElementById("nom").value.length<20){
+
+<script type="text/javascript">        
+     function validation() 
+     { 
+         
+         if(document.getElementById("nom").value==""||5=> document.getElementById("nom").value.length)
+         {
             valid = false;
             return valid;
 
          }
 
-         if(document.getElementById("Prénom").value==""||5=< document.getElementById("Prénom").value.length<20){
+         if(document.getElementById("Prénom").value==""||5=> document.getElementById("Prénom").value.length )
+         {
+            //alert("Le champ nom est obligatoire");
             valid = false;
             return valid;
 
          }
 
-         if(document.getElementById("Adresse Postale").value==""||5=< document.getElementById("Adresse Postale").value.length<50){
+         if(document.getElementById("Adresse Postale").value==""||5=> document.getElementById("Adresse Postale").value.length<50)
+         {
             valid = false;
             return valid;
 
          }
 
-         if(document.getElementById("Mail").value==""||5=< document.getElementById("Mail").value.length<30){
+         if(document.getElementById("Mail").value==""||5=> document.getElementById("Mail").value.length)
+         {
             valid = false;
             return valid;
 
          }
-         if(document.getElementById("date").value=="" ){
+
+         else if (document.getElementById("Mail").indexOf("@") == -1) {
+            alert("Il n'y a pas d'arrobas dans cette adresse") ;
+            valid = false;
+            return valid;
+            } else if (document.getElementById("Mail").indexOf(".") == -1) {
+            alert("Il n'y a pas de point dans cette adresse") ;
+            valid = false;
+            return valid;
+            }
+         if(document.getElementById("date").value=="" )
+
+         {
+            
             valid = false;
             return valid;
 
          }
 
-         if (valid==false) {
+         if (valid==false) 
+         {
             document.getElementById("error").innerHTML = "La saisie du nom est obligatoire";
              
          } else {
