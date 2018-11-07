726c931467a4ec4e4e5b720cad1e55e010677cd6
oueriemmi <44141988+oueriemmi@users.noreply.github.com>
2018-11-07 17:33:18 +0100

    Tp3Essai07/11/2018

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -12,40 +12,32 @@ $(document).ready(function () {
 
 
         if ($('#nom').val().length < 5 || $('#nom').val() == "") { // si la chaîne de caractères est inférieure à 5
-            $('#nom').css({ // on rend le champ rouge
-                borderColor: 'red',
-                color: 'red'
-            });
+            $('#myModal').modal("show");
         }
 
 
         else if ($('#prenom').val().length < 5 || $('#prenom').val() == "") { // si la chaîne de caractères est inférieure à 5
-            $('#prenom').css({ // on rend le champ rouge
-                borderColor: 'red',
-                color: 'red'
-            });
+            $('#myModal').modal("show");
         }
 
 
         else if ($('#date').val().length < 5 || $('#date').val() == "") { // si la chaîne de caractères est inférieure à 5
-            $('#date').css({ // on rend le champ rouge
-                borderColor: 'red',
-                color: 'red'
-            });
+            $('#myModal').modal("show");
         }
 
 
         else if ($('#adresse').val().length < 5 || $('#adresse').val() == "") { // si la chaîne de caractères est inférieure à 5
-            $('#adresse').css({ // on rend le champ rouge
-                borderColor: 'red',
-                color: 'red'
-            });
+            $('#myModal').modal("show");
         }
         else if ($('#mail').val().length < 5 || $('#mail').val() == "") { // si la chaîne de caractères est inférieure à 5
-            $('#mail').css({ // on rend le champ rouge
-                borderColor: 'red',
-                color: 'red'
-            });
+            $('#myModal').modal("show");
+        }
+        else {
+            $('#myModal2').modal("show");
+            document.getElementById('welcome').innerHTML= "Bienvenue  "+ document.querySelector("#prenom").value;
+            document.getElementById('message').innerHTML= "Vous etes nés le  "+ document.querySelector("#date").value +" et vous habitez ";
+            document.getElementById('message2').innerHTML=  document.querySelector("#adresse").value ;
+
         }
 
 
