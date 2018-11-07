cb7914ae0904b1a50e64337be9b79f39fb8a93fd
oueriemmi <44141988+oueriemmi@users.noreply.github.com>
2018-11-07 12:28:10 +0100

    tp3TEST

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,30 +1,58 @@
-$( document ).ready(function() {
-    
-    // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
-    // voir plus : https://www.w3schools.com/js/js_htmldom.asp
-     console.log( "DOM ready!" );
-     
-     // Y mettre le code jQuery pour valider tous les champs du formulaire
+$(document).ready(function () {
+    $("#Valider").on("click", function (e) {
+        e.preventDefault();
 
-     var $nom = $('#nom'),
-         $prenom = $('#prenom'),
-         $date = $('#date'),
-         $adresse = $('#adresse'),
-         $mail = $('#mail'),
-         $champ = $('.champ');
+        // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+        // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+        console.log("DOM ready!");
 
-         if($(this).val().length < 5|| $(this).val()==""){ // si la chaîne de caractères est inférieure à 5
-            $(this).css({ // on rend le champ rouge
-                borderColor : 'red',
-            color : 'red'
+        // Y mettre le code jQuery pour valider tous les champs du formulaire
+
+
+
+
+        if ($('#nom').val().length < 5 || $('#nom').val() == "") { // si la chaîne de caractères est inférieure à 5
+            $('#nom').css({ // on rend le champ rouge
+                borderColor: 'red',
+                color: 'red'
             });
-         }
-         else{
-            $(this).css({ // si tout est bon, on le rend vert
-            borderColor : 'green',
-            color : 'green'
-        });
         }
 
- });
- 
+
+        else if ($('#prenom').val().length < 5 || $('#prenom').val() == "") { // si la chaîne de caractères est inférieure à 5
+            $('#prenom').css({ // on rend le champ rouge
+                borderColor: 'red',
+                color: 'red'
+            });
+        }
+
+
+        else if ($('#date').val().length < 5 || $('#date').val() == "") { // si la chaîne de caractères est inférieure à 5
+            $('#date').css({ // on rend le champ rouge
+                borderColor: 'red',
+                color: 'red'
+            });
+        }
+
+
+        else if ($('#adresse').val().length < 5 || $('#adresse').val() == "") { // si la chaîne de caractères est inférieure à 5
+            $('#adresse').css({ // on rend le champ rouge
+                borderColor: 'red',
+                color: 'red'
+            });
+        }
+        else if ($('#mail').val().length < 5 || $('#mail').val() == "") { // si la chaîne de caractères est inférieure à 5
+            $('#mail').css({ // on rend le champ rouge
+                borderColor: 'red',
+                color: 'red'
+            });
+        }
+
+
+
+
+
+       
+
+    });
+});
