ff383bed1a4b08b9dd5d4db29f0a5f1d6a37f734
oueriemmi <44141988+oueriemmi@users.noreply.github.com>
2019-01-14 12:06:46 +0100

    tp6

--- tp6/meteo-angular/src/app/app.module.ts
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -6,17 +6,45 @@ import { AppComponent } from './app.component';
 import { MeteoComponent } from './meteo/meteo.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { MeteoDetailComponent } from './meteo-detail/meteo-detail.component';
+
+
+
+const appRoutes: Routes = [
+  {
+    path: 'meteo/:name',  // la page  affichant la météo prendra comme paramètre 'name'
+    component: MeteoDetailComponent
+  }, // Ce component fera l'appel AJAX et afficher les données reçues par openWeatherMap
+  {
+    path: '', // un chemin vide renverra vers '/'
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  {
+    path: '', // la page principale utilisera le component suivant
+    component: MeteoComponent
+  }
+];
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeteoComponent
+    MeteoComponent,
+    MeteoDetailComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+    // autres imports ici
+    ,FormsModule,
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tp6/meteo-angular/src/app/services/meteo.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { MeteoItem } from '../meteoItem';
+
+@Injectable()
+export class MeteoService {
+
+  constructor() { }
+
+
+  getMeteo(name: string): Promise<any> {
+    console.log('from service', name);
+
+    let m = new MeteoItem();
+
+    return fetch('https://demo.bilelz.fr/owmap/?q=' + name + '&units=metric&lang=fr&appid=77213358028c4e03362f4c884f35d445')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+
+        // test du code retour
+        // 200 = OK
+        // 404 = city not found 
+        if (json.cod === 200) {
+          return Promise.resolve(json);
+        } else {
+          m.weather = json;
+
+          console.error('Météo introuvable pour ' + name
+            + ' (' + json.message + ')');
+
+          return Promise.reject('Météo introuvable pour ' + name
+          + ' (' + json.message + ')');
+        }
+
+      });
+
+  }
+}
